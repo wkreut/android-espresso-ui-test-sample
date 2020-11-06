a7d147bd7b05a72964a5d69bcd3cd29ae9128823
wkreut <49719278+wkreut@users.noreply.github.com>
2020-11-06 16:09:25 +0000

    Update bitbar.sh

--- bitbar.sh
+++ bitbar.sh
@@ -1,4 +1,4 @@
-curl -u 'YOUR_API_KEY_HERE:' \
+curl -u '0dab7bc8f183e451adde3ad2f853301c:' \
 -H 'Content-Type: application/json' \
 -d '{"osType":"ANDROID","projectId":null,"loadedPrevious":false,"files":[{"id":131757413,"action":"INSTALL"},{"id":129969039,"action":"RUN_TEST"}],"useSamples":false,"deviceGroupId":14,"frameworkId":252,"applicationUsername":null,"applicationPassword":null,"deviceLanguageCode":"en_US","hookURL":null,"instrumentationRunner":null,"limitationType":null,"limitationValue":null,"screenshotDir":null,"withAnnotation":null,"withoutAnnotation":null,"timeout":1800,"projectName":"Bitrise CI","testRunName":null,"testRunParameters":[]}' \
 https://cloud.bitbar.com/cloud/api/v2/me/runs
