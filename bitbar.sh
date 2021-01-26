69ba0e379da8e1cb548b33340d7251c708a5d8c6
wkreut <49719278+wkreut@users.noreply.github.com>
2021-01-26 13:11:32 +0000

    Update bitbar.sh

--- bitbar.sh
+++ bitbar.sh
@@ -1,3 +1,5 @@
+python --version
+
 curl -u $API_KEY: \
 -H 'Content-Type: application/json' \
 -d '{"osType":"ANDROID","projectId":207047951,"loadedPrevious":false,"files":[{"id":131757413,"action":"INSTALL"},{"id":129969039,"action":"RUN_TEST"}],"useSamples":false,"deviceGroupId":14,"frameworkId":252,"applicationUsername":null,"applicationPassword":null,"deviceLanguageCode":"en_US","hookURL":null,"instrumentationRunner":null,"limitationType":null,"limitationValue":null,"screenshotDir":null,"withAnnotation":null,"withoutAnnotation":null,"timeout":1800,"testRunName":null,"testRunParameters":[]}' \
